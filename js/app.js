3de9f020808c79831069aed822025f12148859d5
David Rabkin <davidrabkin@gmail.com>
2014-11-25 01:28:13 -0800

    Update quotes format, change icon on scroll

--- js/app.js
+++ js/app.js
@@ -1,48 +1,48 @@
 var quotes = [
-	["Life is like riding a bicycle: you don't fall off unless you stop pedaling", "Claude Pepper"],
-	["Life is like riding a bicycle. To keep your balance you must keep moving.", "Albert Einstein"],
-	["I thought of that while riding my bicycle.", "Albert Einstein"],
-	["If I can bicycle, I bicycle.", "David Attenborough"],
-	["I find buying a bicycle is a great way to stay in touch with people.", "Jan Chipchase"],
-	["A bicycle has transformed my experience of London.", "Iain Glen"],
-	["The bicycle is a curious vehicle. Its passenger is its engine", "John Howard"],
-	["Bicycles are almost as good as guitars for meeting girls.", "Bob Weir"],
-	["When man invented the bicycle he reached the peak of his attainments.", "Elizabeth West"],
-	["Ever bike? Now that's something that makes life worth living!", "Jack London"],
-	["Get a bicycle. You will certainly not regret it, if you live.", "Mark Twain"],
-	["Cycle tracks will abound in Utopia.", "H.G. Wells"],
-	["It never gets easier, you just go faster", "Greg leMond"],
-	["When my legs hurt, I say: 'Shut up legs! Do what I tell you to do!'", "Jens Voigt"],
-	["If it hurts me, it must hurt the other ones twice as much.", "Jens Voigt"],
-	["Crashing is part of cycling as crying is part of love.", "Johan Museeuw"],
-	["Melancholy is incompatible with bicycling.", "James E. Starrs"],
-	["Nothing compares to the simple pleasure of riding a bike.", "John F. Kennedy"],
-	["Think of bicycles as rideable art that can just about save the world.", "Grant Petersen"],
-	["It is the unknown around the corner that turns my wheels.", "Heinz Stucke"],
-	["The bicycle is the noblest invention of mankind.", "William Saroyan"],
-	["Bicycles are the indicator species of a community, like shellfish in a bay.", "P. Martin Scott"],
-	["Whoever invented the bicycle deserves the thanks of humanity.", "Lord Charles Beresford"],
-	["For every uphill, there's a downhill.", "Mike Hodges"],
-	["Biking is good for the brain, and not bad for the ass either.", "Citizen Chain Cyclery"],
-	["Set your cycling goals high and dont stop until you get there.", "Felicity Luckey"],
-	["Its not the mountain we conquer but ourselves.", "Sir Edmund Hillary"],
-	["To bike, or not to bike: That is not a question!", "Unknown"],
-	["Our chains set us free.", "Unknown"],
-	["Feet in the pedals, Head in the clouds.", "Cycology"],
-	["The more you dream, the farther you get.", "Michael Phelps"],
-	["Bicycling is the nearest approximation I know to the flight of birds.", "Louis J. Helle, Jr."],
-	["Cycling in the city, and particularly in midtown, is anarchy without malice.", "Unknown"],
-	["With highwheelers a flesh and blood man can hitch wings to his feet.", "Karl Kron"],
-	["Bicycles may change, but cycling is timeless.", "Zapata Espinoza"],
-	["Bicycles have no walls.", "Paul Cornish"],
-	["The bicycle is the noblest invention of mankind.", "William Saroyan"],
-	["A bicycle ride around the world begins with a single pedal stroke.", "Scott Stoll"],
-	["Work to eat. Eat to live. Live to bike. Bike to work.", "Unknown"],
-	["A raggedy ride beats a dressed up walk.", "Simon Peat"],
-	["Ride as much or as little, or as long or as short as you feel. But ride.", "Eddy Merckx"],
-	["A mountain bike is like your buddy. A road bike is your lover.", "Sean Coffey"],
-	["Life may not be about your bike, but it sure can help you get through it.", "Hallman"],
-	["I ride my bicycle to ride my bicycle", "Unknown"]
+  ["Life is like riding a bicycle: you don't fall off unless you stop pedaling", "Claude Pepper"],
+  ["Life is like riding a bicycle. To keep your balance you must keep moving.", "Albert Einstein"],
+  ["I thought of that while riding my bicycle.", "Albert Einstein"],
+  ["If I can bicycle, I bicycle.", "David Attenborough"],
+  ["I find buying a bicycle is a great way to stay in touch with people.", "Jan Chipchase"],
+  ["A bicycle has transformed my experience of London.", "Iain Glen"],
+  ["The bicycle is a curious vehicle. Its passenger is its engine", "John Howard"],
+  ["Bicycles are almost as good as guitars for meeting girls.", "Bob Weir"],
+  ["When man invented the bicycle he reached the peak of his attainments.", "Elizabeth West"],
+  ["Ever bike? Now that's something that makes life worth living!", "Jack London"],
+  ["Get a bicycle. You will certainly not regret it, if you live.", "Mark Twain"],
+  ["Cycle tracks will abound in Utopia.", "H.G. Wells"],
+  ["It never gets easier, you just go faster", "Greg leMond"],
+  ["When my legs hurt, I say: 'Shut up legs! Do what I tell you to do!'", "Jens Voigt"],
+  ["If it hurts me, it must hurt the other ones twice as much.", "Jens Voigt"],
+  ["Crashing is part of cycling as crying is part of love.", "Johan Museeuw"],
+  ["Melancholy is incompatible with bicycling.", "James E. Starrs"],
+  ["Nothing compares to the simple pleasure of riding a bike.", "John F. Kennedy"],
+  ["Think of bicycles as rideable art that can just about save the world.", "Grant Petersen"],
+  ["It is the unknown around the corner that turns my wheels.", "Heinz Stucke"],
+  ["The bicycle is the noblest invention of mankind.", "William Saroyan"],
+  ["Bicycles are the indicator species of a community, like shellfish in a bay.", "P. Martin Scott"],
+  ["Whoever invented the bicycle deserves the thanks of humanity.", "Lord Charles Beresford"],
+  ["For every uphill, there's a downhill.", "Mike Hodges"],
+  ["Biking is good for the brain, and not bad for the ass either.", "Citizen Chain Cyclery"],
+  ["Set your cycling goals high and dont stop until you get there.", "Felicity Luckey"],
+  ["Its not the mountain we conquer but ourselves.", "Sir Edmund Hillary"],
+  ["To bike, or not to bike: That is not a question!", "Unknown"],
+  ["Our chains set us free.", "Unknown"],
+  ["Feet in the pedals, Head in the clouds.", "Cycology"],
+  ["The more you dream, the farther you get.", "Michael Phelps"],
+  ["Bicycling is the nearest approximation I know to the flight of birds.", "Louis J. Helle, Jr."],
+  ["Cycling in the city, and particularly in midtown, is anarchy without malice.", "Unknown"],
+  ["With highwheelers a flesh and blood man can hitch wings to his feet.", "Karl Kron"],
+  ["Bicycles may change, but cycling is timeless.", "Zapata Espinoza"],
+  ["Bicycles have no walls.", "Paul Cornish"],
+  ["The bicycle is the noblest invention of mankind.", "William Saroyan"],
+  ["A bicycle ride around the world begins with a single pedal stroke.", "Scott Stoll"],
+  ["Work to eat. Eat to live. Live to bike. Bike to work.", "Unknown"],
+  ["A raggedy ride beats a dressed up walk.", "Simon Peat"],
+  ["Ride as much or as little, or as long or as short as you feel. But ride.", "Eddy Merckx"],
+  ["A mountain bike is like your buddy. A road bike is your lover.", "Sean Coffey"],
+  ["Life may not be about your bike, but it sure can help you get through it.", "Hallman"],
+  ["I ride my bicycle to ride my bicycle", "Unknown"]
 ];
 
 var lastQuoteStanding = ["Damn, bicycling is awesome.", "Everyone"];
@@ -63,3 +63,18 @@ var quoteGen = function(){
 }
 
 $(document).ready(function(){quoteGen()})
+
+$(window).scroll(function(){
+  if ($(window).scrollTop() > 320){
+  		console.log(document.getElementById("shareIcon"));
+  		//console.log(document.getElementById("shareIcon").data);
+  		document.getElementById("shareIcon").data = "img/SVGbrand_spocket_logotypewsymbol.svg";
+      	document.getElementById("shareIcon").class = "";
+      	//$("#shareicon").removeClass('shareicon').removeClass('navbarimg').addClass('logo'); 
+      	//document.getElementById("shareIcon").parent().class = "small-3 column left";
+      //(function(){console.log("We're at the bottom of the page!!")}, 500);
+  } else {
+  	document.getElementById("shareIcon").data = "img/SVGic_share_AND.svg";
+  	//document.getElementById("shareIcon").class = "navbarimg svg";
+  }
+});
--- js/rss.js
+++ js/rss.js
@@ -33,51 +33,41 @@ function initialize() {
 google.setOnLoadCallback(initialize);
 
 disableScroll();
-    setInterval(function(){ if(loaded === true){enableScroll(); clearInterval(); }}, 100);
 
-$(document).ready(function(){
-    var keys = [37, 38, 39, 40];
+var keys = [37, 38, 39, 40];
 
-    function preventDefault(e) {
-      e = e || window.event;
-      if (e.preventDefault)
-          e.preventDefault();
-      e.returnValue = false;  
-    }
+function preventDefault(e) {
+  e = e || window.event;
+  if (e.preventDefault)
+      e.preventDefault();
+  e.returnValue = false;  
+}
 
-    function keydown(e) {
-      for (var i = keys.length; i--;) {
-        if (e.keyCode === keys[i]) {
-            preventDefault(e);
-            return;
-            }
+function keydown(e) {
+  for (var i = keys.length; i--;) {
+    if (e.keyCode === keys[i]) {
+        preventDefault(e);
+        return;
         }
     }
+}
 
-    function wheel(e) {
-      preventDefault(e);
+function wheel(e) {
+  preventDefault(e);
+}
+
+function disableScroll() {
+  if (window.addEventListener) {
+      window.addEventListener('DOMMouseScroll', wheel, false);
+  }
+  window.onmousewheel = document.onmousewheel = wheel;
+}
+
+function enableScroll() {
+    if (window.removeEventListener) {
+        window.removeEventListener('DOMMouseScroll', wheel, false);
     }
+    window.onmousewheel = document.onmousewheel = null;  
+}
 
-    function disableScroll() {
-      if (window.addEventListener) {
-          window.addEventListener('DOMMouseScroll', wheel, false);
-      }
-      window.onmousewheel = document.onmousewheel = wheel;
-    }
-
-    function enableScroll() {
-        if (window.removeEventListener) {
-            window.removeEventListener('DOMMouseScroll', wheel, false);
-        }
-        window.onmousewheel = document.onmousewheel = null;  
-    }
-});
-
-
-
-
-
-
-
-
-
+setInterval(function(){ if(loaded === true){enableScroll(); clearInterval(); }}, 100);
