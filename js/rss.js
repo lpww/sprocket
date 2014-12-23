46f697a904d2c9c9902cac671abb61674b1d7e5c
David Rabkin <davidrabkin@gmail.com>
2014-12-22 17:52:23 -0800

    Format rss.js

--- js/rss.js
+++ js/rss.js
@@ -1,23 +1,29 @@
-google.load("feeds", "1");
+//formats the rss feed
 
-var loaded = false;
+google.load("feeds", "1");
 
 function initialize() {
   var feed = new google.feeds.Feed("http://widget.websta.me/rss/n/sprocketblog");
-  feed.setNumEntries(21);
+  feed.setNumEntries(2);
   feed.load(function(result) {
     if (!result.error) {
       var container = document.getElementById("feed");
       for (var i = 0; i < result.feed.entries.length; i++) {
         var entry = result.feed.entries[i];
+        console.log(entry);
+
         var cardDiv = document.createElement("div");
         cardDiv.className = "card";
+        
         var imageDiv = document.createElement("div");
         imageDiv.className = "image";
+        
         var textDiv = document.createElement("div");
         textDiv.className = "text";
+        
         var commentsAndLikesDiv = document.createElement("div");
         commentsAndLikesDiv.className = "commentsAndLikes"
+        
         var data = entry.content.slice(entry.content.indexOf("@sprocketblog</a></p>") + 24, entry.content.indexOf("<img src=\"http://websta.me/img/tags_mono.png")).replace('width="306" height="306"', "").replace("_a.", "_n.");
         var rssData = data.split("</p>");
         imageDiv.innerHTML = rssData[0];
@@ -32,6 +38,7 @@ function initialize() {
         var likesBorder = document.createElement("div");
         likesBorder.className = "iconBorder";
         likesBorder.innerHTML = "<span>" + x.slice(x.indexOf('<img src="img/SVGic_comments.svg"')) + "</span>";
+        
         cardDiv.appendChild(imageDiv);
         cardDiv.appendChild(textDiv);
         cardDiv.appendChild(commentsAndLikesDiv);
@@ -47,45 +54,3 @@ function initialize() {
 };
 
 google.setOnLoadCallback(initialize);
-
-
-
-// disableScroll();
-
-// var keys = [37, 38, 39, 40];
-
-// function preventDefault(e) {
-//   e = e || window.event;
-//   if (e.preventDefault)
-//       e.preventDefault();
-//   e.returnValue = false;  
-// }
-
-// function keydown(e) {
-//   for (var i = keys.length; i--;) {
-//     if (e.keyCode === keys[i]) {
-//         preventDefault(e);
-//         return;
-//         }
-//     }
-// }
-
-// function wheel(e) {
-//   preventDefault(e);
-// }
-
-// function disableScroll() {
-//   if (window.addEventListener) {
-//       window.addEventListener('DOMMouseScroll', wheel, false);
-//   }
-//   window.onmousewheel = document.onmousewheel = wheel;
-// }
-
-// function enableScroll() {
-//     if (window.removeEventListener) {
-//         window.removeEventListener('DOMMouseScroll', wheel, false);
-//     }
-//     window.onmousewheel = document.onmousewheel = null;  
-// }
-
-// setInterval(function(){ if(loaded === true){enableScroll(); clearInterval(); }}, 100);
